42bc8be6a62b08a01301f175c9e9777dfa75e0d1
fehmior <114577025+fehmior@users.noreply.github.com>
2022-10-05 11:05:49 +0200

    Create KartaPracy2.cs

---
+++ KartaPracy2.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           /* Zad1
+            int a = int.Parse(Console.ReadLine());
+            if (a % 3 ==0)
+            {
+                Console.WriteLine("TAK");
+            }
+            else
+            {
+                Console.WriteLine("Nie");
+            }*/
+
+            /*Zad2
+            int a = int.Parse(Console.ReadLine());
+            if (a>=100 && a< 1000 && a%17==0)
+            {
+                Console.WriteLine("Tak");
+            }
+            else
+            {
+                Console.WriteLine("NIE");
+            }*/
+
+            /*Zad3
+            int a = int.Parse(Console.ReadLine());
+            if (a>=18)
+            {
+                Console.WriteLine("Tak");
+            }
+            else
+            {
+                Console.WriteLine("NIE");
+            }*/
+
+            
+            /*Zad4
+            Console.WriteLine("podaj wage");
+            int waga = int.Parse(Console.ReadLine());
+            const int limit = 20;
+            if (waga>limit)
+            {
+                Console.WriteLine("Nie może wjechac");
+            }
+            else
+            {
+                Console.WriteLine("Moze wjechac");
+            }*/
+
+            /*Zad5
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            if (a < c && b > c && b < c && a > c)
+            {
+                Console.WriteLine("Miesci");
+            }
+            else
+            {
+                Console.WriteLine("nie miesci sie");
+            }*/
+
+            /*Zad6
+            int a = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+            if ((Math.Pow(a,p) -a) % p ==0)
+            {
+                Console.WriteLine("Tak");
+            }
+            else
+            {
+                Console.WriteLine("Nie");
+            }*/
+
+           /* Zad7
+            int p = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int s = int.Parse(Console.ReadLine());
+            if ((p + 3 * s) >= k)
+            {
+                Console.WriteLine("TAK, zaba da rade");
+            }
+            else
+            {
+                Console.WriteLine("NIE, nie da rady");
+            }
+            Console.ReadKey();*/
+
+        }
+    }
+}
